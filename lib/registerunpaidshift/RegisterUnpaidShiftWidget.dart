2d50f6a75f11e0c1bc2f66a26bb56dec2811ef79
miguel <mig.ruiz@gmail.com>
2022-01-23 21:47:42 +0000

    setting time

--- lib/registerunpaidshift/RegisterUnpaidShiftWidget.dart
+++ lib/registerunpaidshift/RegisterUnpaidShiftWidget.dart
@@ -21,86 +21,96 @@ class RegisterUnpaidShiftWidget extends StatelessWidget{
                 ),
                 body: Column(
                   children: [
-                    state.startDateTimeSet?Text(
-                      state.start!
-                          .formatDateTime()
-                          .toCapitalized(),
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16),
-                    ):ElevatedButton(
-                      onPressed: () async{
-                        final dateResult = await showDatePicker(
-                          initialEntryMode: DatePickerEntryMode.calendar,
-                          context: context,
-                          helpText: 'FECHA DE ENTRADA',
-                          cancelText: "CANCELAR",
-                          confirmText: 'SIGUIENTE',
-                          firstDate: DateTime(2020),
-                          initialDate: state.start??DateTime.now(),
-                          lastDate: DateTime(2040),
-                        );
-                        if (dateResult!=null) {
-                          final timeResult = await showTimePicker(
-                            context: context,
-                            helpText: 'HORA DE ENTRADA',
-                            cancelText: 'CANCELAR',
-                            confirmText: 'CONFIRMAR',
-                            initialTime: (state.start ?? DateTime.now())
-                                .timeOfDay(),
-                          );
-                          if (timeResult!=null){
-                            final startDateTime = dateResult.combine(timeValue: timeResult);
-                            bloc.add(StartDateTimeEvent(startDateTime));
-                          }
-                        }
-                      },
-                      child: Text('Fecha y Hora de Entrada',
-                          style: TextStyle(fontSize: 18)),
+                    Align(
+                        alignment: Alignment.center,
+                        child: getStartWidget(state, context, bloc)
                     ),
                     if (state.displayEndDateTimeLine)
-                      state.endDateTimeSet?Text(
-                        state.end!
-                            .formatDateTime()
-                            .toCapitalized(),
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 16),
-                      ):ElevatedButton(
-                        onPressed: () async{
-                          final dateResult = await showDatePicker(
-                            context: context,
-                            helpText: 'FECHA DE SALIDA',
-                            cancelText: "CANCELAR",
-                            confirmText: 'SIGUIENTE',
-                            firstDate: DateTime(2020),
-                            initialDate: state.end??DateTime.now(),
-                            lastDate: DateTime(2040),
-                          );
-                          if (dateResult!=null) {
-                            final timeResult = await showTimePicker(
-                              context: context,
-                              helpText: 'HORA DE SALIDA',
-                              cancelText: 'CANCELAR',
-                              confirmText: 'CONFIRMAR',
-                              initialTime: (state.end ?? DateTime.now())
-                                  .timeOfDay(),
-                            );
-                            if (timeResult!=null){
-                              final endDateTime = dateResult.combine(timeValue: timeResult);
-                              bloc.add(EndDateTimeEvent(endDateTime));
-                            }
-                          }
-                        },
-                        child: Text('Fecha y Hora de Salida',
-                            style: TextStyle(fontSize: 18)),
+                    Align(
+                        alignment: Alignment.center,
+                        child: Text("↓", style: TextStyle(fontSize: 40))
+                    ),
+                    if (state.displayEndDateTimeLine)
+                      Align(
+                          alignment: Alignment.center,
+                          child: getEndWidget(state, context, bloc)
                       )
+
                   ],
                 ),
               );
             }
         ),
       ),
+    );
+  }
+
+  Widget getEndWidget(RegisterUnpaidShiftState state, BuildContext context, RegisterUnpaidShiftBloc bloc)  {
+    return ElevatedButton(
+                      onPressed: () async{
+                        final dateResult = await showDatePicker(
+                          context: context,
+                          helpText: 'FECHA DE SALIDA',
+                          cancelText: "CANCELAR",
+                          confirmText: 'SIGUIENTE',
+                          firstDate: DateTime(2020),
+                          initialDate: state.end??DateTime.now(),
+                          lastDate: DateTime(2040),
+                        );
+                        if (dateResult!=null) {
+                          final timeResult = await showTimePicker(
+                            context: context,
+                            helpText: 'HORA DE SALIDA',
+                            cancelText: 'CANCELAR',
+                            confirmText: 'CONFIRMAR',
+                            initialTime: (state.end ?? DateTime.now())
+                                .timeOfDay(),
+                          );
+                          if (timeResult!=null){
+                            final endDateTime = dateResult.combine(timeValue: timeResult);
+                            bloc.add(EndDateTimeEvent(endDateTime));
+                          }
+                        }
+                      },
+                      child: Text(state.endDateTimeSet?state.end!
+        .formatDateTime()
+        .toCapitalized(): 'FECHA Y HORA DE SALIDA',
+                          style: TextStyle(fontSize: 20)),
+                    );
+  }
+
+  Widget getStartWidget(RegisterUnpaidShiftState state, BuildContext context, RegisterUnpaidShiftBloc bloc)  {
+    return ElevatedButton(
+      onPressed: () async{
+        final dateResult = await showDatePicker(
+          initialEntryMode: DatePickerEntryMode.calendar,
+          context: context,
+          helpText: 'FECHA DE ENTRADA',
+          cancelText: "CANCELAR",
+          confirmText: 'SIGUIENTE',
+          firstDate: DateTime(2020),
+          initialDate: state.start??DateTime.now(),
+          lastDate: DateTime(2040),
+        );
+        if (dateResult!=null) {
+          final timeResult = await showTimePicker(
+            context: context,
+            helpText: 'HORA DE ENTRADA',
+            cancelText: 'CANCELAR',
+            confirmText: 'CONFIRMAR',
+            initialTime: (state.start ?? DateTime.now())
+                .timeOfDay(),
+          );
+          if (timeResult!=null){
+            final startDateTime = dateResult.combine(timeValue: timeResult);
+            bloc.add(StartDateTimeEvent(startDateTime));
+          }
+        }
+      },
+      child: Text(state.startDateTimeSet?state.start!
+          .formatDateTime()
+          .toCapitalized(): 'FECHA Y HORA DE ENTRADA',
+          style: TextStyle(fontSize: 20)),
     );
   }
 
